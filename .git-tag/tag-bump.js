9c5f02ada2240926fd6c893363aaf044b497ba14
sinsir2 <157989693+sinsir2@users.noreply.github.com>
2025-09-17 20:33:43 +0200

    test commit-2

--- .git-tag/tag-bump.js
+++ .git-tag/tag-bump.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { execSync } from "child_process";
-import fs from "fs";
+
 
 function run(cmd) {
   return execSync(cmd, { encoding: "utf8" }).trim();
@@ -17,19 +17,7 @@ function hasCommits() {
 
 function getLastTag() {
   try {
-    const tags = run("git tag --list").split("\n");
-    if (!tags.length) return "v0.0.0";
-
-    return tags.reduce((max, tag) => {
-      const parse = v => v.replace(/^v/, "").split(".").map(Number);
-      const [m1, n1, p1] = parse(max);
-      const [m2, n2, p2] = parse(tag);
-
-      if (m2 > m1) return tag;
-      if (m2 === m1 && n2 > n1) return tag;
-      if (m2 === m1 && n2 === n1 && p2 > p1) return tag;
-      return max;
-    }, tags[0]);
+    return run("git describe --tags --abbrev=0");
   } catch {
     return "v0.0.0";
   }
@@ -39,64 +27,39 @@ function bumpVersion(lastTag, commitMsg) {
   const version = lastTag.replace(/^v/, "");
   let [major, minor, patch] = version.split(".").map(Number);
 
-  const msg = commitMsg.toLowerCase();
+  const msg = commitMsg.trim();
 
-  if (msg.includes("breaking")) {
+  
+  if (/^breaking/i.test(msg)) {
     major++;
     minor = 0;
     patch = 0;
-  } else if (msg.includes("feat")) {
-    minor++;
+  } else if (/^feat/i.test(msg)) {
+	  minor++;
     patch = 0;
-  } else if (msg.includes("fix")) {
-    patch++;
+  } else if (/^fix/i.test(msg)) {
+	  patch++;
   } else {
-    return null;
+	  return null;
   }
 
   return `v${major}.${minor}.${patch}`;
 }
 
 
-
-// function bumpVersion(lastTag, commitMsg) {
-//   const version = lastTag.replace(/^v/, "");
-//   let [major, minor, patch] = version.split(".").map(Number);
-
-//   const msg = commitMsg.trim();
-
-  
-//   if (/^breaking/i.test(msg)) {
-// 	  major++;
-// 	  minor = 0;
-//     patch = 0;
-//   } else if (/^feat/i.test(msg)) {
-// 	  minor++;
-//     patch = 0;
-//   } else if (/^fix/i.test(msg)) {
-// 	  patch++;
-//   } else {
-// 	return null;
-//   }
-
-//   return `v${major}.${minor}.${patch}`;
-// }
-
-
 function createTag(tag) {
   run(`git tag ${tag}`);
-  run(`git push origin ${tag}`);
 }
 
 
 function main() {
+  console.log("Running Main");
   if (!hasCommits()) {
-    console.log("has no commits")
+    console.log("Has no commits");
+  	process.exit(0);
   }
   const lastTag = getLastTag();
-  console.log("last tag: " + lastTag)
   const commitMsg = run("git log -1 --pretty=%B").trim();
-  console.log("last commitMsg: " + commitMsg)
   const newTag = bumpVersion(lastTag, commitMsg);
   if (newTag) {
     createTag(newTag);
